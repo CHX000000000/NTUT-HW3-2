3657d1ee37f4adf1abf26caa32a04d7d5db199f4
CHX000000000 <abc4567647@gmail.com>
2022-10-31 16:11:36 +0800

    111360207

--- 05/05/main.c
+++ 05/05/main.c
@@ -8,10 +8,9 @@ void hanoi(int n, char A, char B, char C);
 
 int main()
 {
-	char A='A', B='B', C='C';
 	int n;
 	scanf("%d", &n);
-	hanoi(n,A,B,C);
+	hanoi(n,'A','B','C');
 	printf("Total = %d\n", (int)pow(2, n)-1);
 	system("pause");
 	return 0;
@@ -19,10 +18,12 @@ int main()
 
 void hanoi(int n, char A, char B, char C)
 {
-	if (n == 1) {
+	if (n == 1) 
+	{
 		printf("Move sheet from %c to %c\n", A, C);
 	}
-	else {
+	else 
+	{
 		hanoi(n - 1, A, C, B);
 		hanoi(1, A, B, C);
 		hanoi(n - 1, B, A, C);
